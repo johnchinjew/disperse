3d92034a58793fd3631758e5ad2cb122a40b607a
John Chin-Jew <jchinjew@gmail.com>
2017-10-09 17:21:44 -0700

    Add the function

---
+++ disvoke.js
@@ -0,0 +1,11 @@
+// Disperses function @fn invocation randomly over time range [@soonest, @latest] inclusive.
+// Cancel @fn by invoking `.cancel()` on the return object.
+// Returns `false` if given invalid arguments
+function disvoke(fn, soonest=5000, latest=soonest+15000) {
+  if (soonest < 0 || latest < soonest)
+    return false
+  const t = setTimeout(fn, soonest + Math.floor(Math.random() * (latest-soonest+1)))
+  return {
+    cancel: () => clearTimeout(t)
+  }
+}
