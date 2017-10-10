94a33fcc51bfef098ed288f4f4db11a0014a102c
John Chin-Jew <jchinjew@gmail.com>
2017-10-09 17:31:14 -0700

    Rename to 'disperse'

--- disperse.js
+++ disperse.js
@@ -1,7 +1,7 @@
 // Disperses function @fn invocation randomly over time range [@soonest, @latest] inclusive.
 // Cancel @fn by invoking `.cancel()` on the return object.
 // Returns `false` if given invalid arguments
-function disvoke(fn, soonest=5000, latest=soonest+15000) {
+function disperse(fn, soonest=5000, latest=soonest+15000) {
   if (soonest < 0 || latest < soonest)
     return false
   const t = setTimeout(fn, soonest + Math.floor(Math.random() * (latest-soonest+1)))
